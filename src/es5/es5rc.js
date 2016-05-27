ad8ab1a49af542e6812fe7151a5f91356ecf36dc
Mike Chau <mike@chau.io>
2016-05-27 15:03:34 -0500

    es5 config: space-before-function-paren anonymous set to never

--- src/es5/es5rc.js
+++ src/es5/es5rc.js
@@ -3,5 +3,12 @@
 module.exports = {
   extends: [
     'airbnb-base/legacy'
-  ]
+  ],
+
+  rules: {
+    'space-before-function-paren': [2, {
+      anonymous: 'never',
+      named: 'never'
+    }]
+  }
 };
--- src/util/buildConfig.js
+++ src/util/buildConfig.js
@@ -1,23 +1,33 @@
 'use strict';
 
 var omit = require('lodash/omit');
+var merge = require('lodash/merge');
 var getEslintConfig = require('./getEslintConfig');
 var warnitizer = require('./warnitizer');
 
-module.exports = function buildConfig(configModule, params) {
+module.exports = function buildConfig(configModule, params, configParams) {
   var opts = params || {};
   var withWarning = (opts.warning === undefined) ? false : opts.warning;
+
+  var configOpts = configParams || {};
 
   var rawConfig = getEslintConfig(configModule);
   var config = omit(rawConfig, ['extends', 'globals']);
 
-  var results = {
+  var initialResults = {
     base: config
   };
 
   if (withWarning) {
-    results.warning = warnitizer(config);
+    initialResults.warning = warnitizer(config);
   }
 
-  return results;
+  return (
+    Object.keys(initialResults).reduce(function mergeConfig(obj, key) {
+      var eslintConfig = {};
+      eslintConfig[key] = merge({}, initialResults[key], configOpts);
+
+      return merge({}, obj, eslintConfig);
+    }, {})
+  );
 };
